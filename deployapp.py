577fac1a286fa5918bc919ea9fa0a4435ce2ed32
Steve Taylor <steve@deployhub.com>
2020-01-03 12:04:07 -0700

    add deploy container

--- deployapp.py
+++ deployapp.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python3
 
-from pprint import pprint
+import sys
 import click
 
 from deployhub import dhapi
@@ -11,16 +11,8 @@ from deployhub import dhapi
 @click.option('--dhuser', help='DeployHub User', required=True)
 @click.option('--dhpass', help='DeployHub Password', required=True)
 @click.option('--appname', help='Application Name', required=True)
-@click.option('--appversion', help='Application Version')
-@click.option('--compname', help='Component Name', required=True)
-@click.option('--compvariant', help='Component Variant')
-@click.option('--compversion', help='Component Version')
-@click.option('--docker', 'kind', flag_value='docker',
-              default=True, help='Component Item Type')
-@click.option('--file', 'kind', flag_value='file')
-@click.option('--compattr', help='Component Attribute', multiple=True)
-@click.option('--env', help='Environments', multiple=True)
-def main(dhurl, dhuser, dhpass, appname, appversion, compname, compvariant, compversion, kind, env, compattr):
+@click.option('--env', help='Environment', required=True)
+def main(dhurl, dhuser, dhpass, appname, env):
 
     print("Logging into DeployHub")
     cookies = dhapi.login(dhurl, dhuser, dhpass)
@@ -28,34 +20,20 @@ def main(dhurl, dhuser, dhpass, appname, appversion, compname, compvariant, comp
     if cookies is None:
         return
 
-    if (compvariant is None):
-        compvariant = ""
+    # Deploy Application to Environment
+    print(f'Deploying {appname} to {env}')
+    deployid = dhapi.deploy_application(dhurl, cookies, appname, env)
 
-    # create component version
-    print("Creating Component")
-    compid = dhapi.new_component_version(dhurl, cookies, compname, compvariant, compversion, kind, None)
-    print("Creation Done: " + str(compid))
+    print("Fetching Logs")
+    data = dhapi.get_logs(dhurl, cookies, deployid)
 
-    attrs = {}
-    print("Updating Component Attributes\n")
-    for attr in compattr:
-        (key, value) = attr.split(':', 1)
-        attrs[key] = value
-
-    pprint(attrs)
-    print("")
-
-    data = dhapi.update_component_attrs(dhurl, cookies, compname, "", compversion, attrs)
-    print("Attribute Update Done")
-
-    print("Creating Application Version")
-    data = dhapi.new_application(dhurl, cookies, appname, appversion, env)
-    appid = data[0]
-    print("Creation Done: " + str(appid))
-
-    print("Assigning Component Version to Application Version")
-    data = dhapi.add_compver_to_appver(dhurl, cookies, appid, compid)
-    print("Assignment Done")
+    print(data[1])
+    if (data[0]):
+        print("Successful")
+        sys.exit(0)
+    else:
+        print("Failed")
+        sys.exit(1)
 
 
 if __name__ == '__main__':
