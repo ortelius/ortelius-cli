0e3cb3b75e37e900996e3ec58199bd0db9eebb21
Steve Taylor <steve@deployhub.com>
2020-01-08 13:51:16 -0700

    upload project to pypi

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+    setup_requires=['wheel'],
+    url='https://www.deployhub.com',
+    author='Steve Taylor',
+    author_email='steve@deployhub.com',
+    name='deployhub',
+    version='9.0.0',
+    packages=['deployhub',],
+    scripts=['bin/dh'],
+    license='GNU AFFERO GENERAL PUBLIC LICENSE',
+    long_description=open('README').read(),
+    python_requires='>=3.6',
+     install_requires=[
+        'click',
+        'qtoml',
+        'yaml',
+        'requests'
+        ],
+)
