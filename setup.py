89461b49cd8662f75a3505b46770a328929916c4
Steve Taylor <steve@deployhub.com>
2020-02-20 14:35:23 -0700

    Bump version: 9.3.10 → 9.3.11

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Steve Taylor',
     author_email='steve@deployhub.com',
     name='deployhub',
-    version='9.3.10',
+    version='9.3.11',
     packages=['deployhub',],
     scripts=['bin/dh'],
     license='GNU AFFERO GENERAL PUBLIC LICENSE',
