141f29e521e437d4a58c2d6572d5b9aa7fd75620
Steve Taylor <steve@deployhub.com>
2021-01-28 12:14:40 -0700

    Bump version: 9.3.105 → 9.3.106

--- deployhub/__init__.py
+++ deployhub/__init__.py
@@ -1,3 +1,3 @@
 """DeployHub REST API Client module"""
 
-__version__ = '9.3.105'
+__version__ = '9.3.106'
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Steve Taylor',
     author_email='steve@deployhub.com',
     name='deployhub',
-    version='9.3.105',
+    version='9.3.106',
     packages=['deployhub',],
     scripts=['bin/dh'],
     license='GNU AFFERO GENERAL PUBLIC LICENSE',
