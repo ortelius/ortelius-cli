de59b302604c5428e0c2fcabb808df723edc7039
Steve Taylor <steve@deployhub.com>
2021-05-26 16:18:41 -0600

    add links in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 """Setup config for the module."""
 
-from distutils.core import setup
 import setuptools
+
+from distutils.core import setup
 
 setup(
     setup_requires=['wheel'],
-    url='https://www.deployhub.com',
+    url='https://ortelius.io',
+    project_urls={ 
+        'Project Repo': 'https://github.com/ortelius/compupdate',
+        'Issues': 'https://github.com/ortelius/ortelius/issues',
+        'CLI Documentation': 'https://github.com/ortelius/compupdate/blob/master/doc/dh.md',
+        'Python API Documentation': 'https://github.com/ortelius/compupdate/blob/master/doc/deployhub.md'
+        }, 
     author='Steve Taylor',
     author_email='steve@deployhub.com',
     name='deployhub',
@@ -13,7 +20,7 @@ setup(
     packages=['deployhub',],
     scripts=['bin/dh'],
     license='GNU AFFERO GENERAL PUBLIC LICENSE',
-    long_description=open('README').read(),
+    long_description=open('doc/dh.md').read(),
     python_requires='>=3.6',
      install_requires=[
         'click',
@@ -23,4 +30,5 @@ setup(
         'configobj',
         'flatten_dict'
         ],
+    include_package_data=True
 )
