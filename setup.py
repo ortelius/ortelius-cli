6695cb4afe0d475dfbc12ded65ad7a2c8d873539
Steve Taylor <steve@deployhub.com>
2021-06-29 09:21:14 -0600

    Bump version: 9.3.142 → 9.3.143

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     author='Steve Taylor',
     author_email='steve@deployhub.com',
     name='deployhub',
-    version='9.3.142',
+    version='9.3.143',
     packages=['deployhub',],
     scripts=['bin/dh', 'bin/helminfo.sh'],
     license='GNU AFFERO GENERAL PUBLIC LICENSE',
