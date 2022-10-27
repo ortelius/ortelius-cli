cd401f72fd67c89973a7b8f25d2521b4493907d6
Steve Taylor <steve@deployhub.com>
2022-10-27 14:36:32 -0600

    Bump version: 9.3.212 → 9.3.213

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     author='Steve Taylor',
     author_email='steve@deployhub.com',
     name='ortelius-cli',
-    version='9.3.212',
+    version='9.3.213',
     packages=['deployhub',],
     scripts=['bin/dh', 'bin/helminfo.sh'],
     license='Apache-2.0',
