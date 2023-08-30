f3fcc0e5c464597e4276d912581788e4dad92892
Steve Taylor <steve@deployhub.com>
2023-08-30 14:42:45 -0600

    remove namespace Signed-off-by: Steve Taylor <steve@deployhub.com>

--- deployhub/dhapi.py
+++ deployhub/dhapi.py
@@ -72,7 +72,7 @@ def get_json(url, cookies):
 
     """
     try:
-        res = requests.get(url, cookies=cookies, timeout=300)
+        res = requests.get(url, cookies=cookies, timeout=30)
         if res is None:
             return None
         if res.status_code != 200:
@@ -98,7 +98,7 @@ def post_json(url, payload, cookies):
         string: The json string.
     """
     try:
-        res = requests.post(url, data=payload, cookies=cookies, headers={"Content-Type": "application/json"}, timeout=300)
+        res = requests.post(url, data=payload, cookies=cookies, headers={"Content-Type": "application/json"}, timeout=30)
         if res is None:
             return None
         if res.status_code != 200:
@@ -186,7 +186,7 @@ def login(dhurl, user, password, errors):
         string: the cookies to be used in subsequent API calls.
     """
     try:
-        result = requests.post(dhurl + "/dmadminweb/API/login", data={"user": user, "pass": password}, timeout=300)
+        result = requests.post(dhurl + "/dmadminweb/API/login", data={"user": user, "pass": password}, timeout=30)
         cookies = result.cookies
         if result.status_code == 200:
             data = result.json()
@@ -2018,7 +2018,7 @@ def post_textfile(dhurl, cookies, compid, filename, file_type):
         file_data = open(filename, "rb").read()
     else:
         try:
-            res = requests.get(filename, timeout=300)
+            res = requests.get(filename, timeout=30)
             if res.status_code == 200:
                 file_data = res.content
         except requests.exceptions.ConnectionError:
