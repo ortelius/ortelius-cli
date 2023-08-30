906d83979d335c0984d499785bf09c2fb83dee6c
Steve Taylor <steve@deployhub.com>
2023-08-30 11:53:44 -0600

    add verify=False for ssl Signed-off-by: Steve Taylor <steve@deployhub.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author="Steve Taylor",
     author_email="steve@deployhub.com",
     name="ortelius-cli",
-    version="9.3.226",
+    version="9.3.229",
     packages=[
         "deployhub",
     ],
