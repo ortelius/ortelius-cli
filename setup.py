702c208ec4b60260343befde4569d0b86b031e0e
Steve Taylor <steve@deployhub.com>
2024-06-20 13:32:25 -0600

    Bump version: 9.3.274 → 9.3.275 Signed-off-by: Steve Taylor <steve@deployhub.com>

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     author="Steve Taylor",
     author_email="steve@deployhub.com",
     name="ortelius-cli",
-    version="9.3.274",
+    version="9.3.275",
     packages=[
         "deployhub",
     ],
